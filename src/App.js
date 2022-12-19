0b4fefec0f51ddee58180fe372df9fa1e6a975f3
abdalhak1m <abdalhakimalhallak6@gmail.com>
2022-12-19 19:02:13 +0300

    create variables and Change Avatar Class

--- src/App.js
+++ src/App.js
@@ -9,7 +9,33 @@ function App() {
     songSrc: './Assets/songs/Chasing - NEFFEX.mp3',
     songAvatar: './Assets/Images/image1.jpg'
   })
-  
+
+  //UseStates Variables
+  const [audioProgress, setAudioProgress] = useState(0);
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [musicIndex, setMusicIndex] = useState(0);
+  const [musicTotalLength, setMusicTotalLength] = useState('04 : 38');
+  const [musicCurrentTime, setMusicCurrentTime] = useState('00 : 00');
+  const [videoIndex, setVideoIndex] = useState(0)
+
+  const currentAudio = useRef()
+
+  const handleMusicProgressBar = (e)=>{
+    setAudioProgress(e.target.value);
+    currentAudio.current.currentTime = e.target.value * currentAudio.current.duration / 100;
+  }
+
+  //Change Avatar Class
+  let avatarClass = ['objectFitCover','objectFitContain','none']
+  const [avatarClassIndex, setAvatarClassIndex] = useState(0)
+  const handleAvatar = ()=>{
+    if (avatarClassIndex >= avatarClass.length - 1) {
+      setAvatarClassIndex(0)
+    }else{
+      setAvatarClassIndex(avatarClassIndex + 1)
+    }
+  }
+
 }
 
 export default App;
