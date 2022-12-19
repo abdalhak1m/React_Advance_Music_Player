9a61db557f81089deda04728e272f70235fafc37
abdalhak1m <abdalhakimalhallak6@gmail.com>
2022-12-19 19:07:20 +0300

    create audio play function

--- src/App.js
+++ src/App.js
@@ -36,6 +36,100 @@ function App() {
     }
   }
 
+  //Play Audio Function
+  const handleAudioPlay = ()=>{
+    if (currentAudio.current.paused) {
+      currentAudio.current.play();
+      setIsAudioPlaying(true)
+    }else{
+      currentAudio.current.pause();
+      setIsAudioPlaying(false)
+    }
+  }
+
+  const musicAPI = [
+    {
+      songName: 'Chasing',
+      songArtist: 'NEFFEX',
+      songSrc: './Assets/songs/Chasing - NEFFEX.mp3',
+      songAvatar: './Assets/Images/image1.jpg'
+    },
+    {
+      songName: 'AURORA - Runaway',
+      songArtist: 'Aurora Aksnes',
+      songSrc: './Assets/songs/AURORA - Runaway (Lyrics).mp3',
+      songAvatar: './Assets/Images/image4.jpg'
+    },
+    {
+      songName: 'Catch Me If I Fall',
+      songArtist: 'TEGNENT',
+      songSrc: './Assets/songs/Catch Me If I Fall - NEFFEX.mp3',
+      songAvatar: './Assets/Images/image2.jpg'
+    },
+    {
+      songName: 'Inspired (Clean)',
+      songArtist: 'NEFFEX',
+      songSrc: './Assets/songs/Inspired (Clean) - NEFFEX.mp3',
+      songAvatar: './Assets/Images/image3.jpg'
+    },
+    {
+      songName: 'Baby doll [ slowed + reverb ]',
+      songArtist: 'Kanika Kapoor',
+      songSrc: './Assets/songs/Baby doll [ slowed + reverb ] __ meet bros ,Kanika Kapoor __ jr santu.mp3',
+      songAvatar: './Assets/Images/image5.jpg'
+    },
+    {
+      songName: 'Soch (Slowed+Reverbed)',
+      songArtist: 'Hardy Sandhu',
+      songSrc: './Assets/songs/SOCH(Slowed+Reverbed) __ Hardy Sandhu.webm',
+      songAvatar: './Assets/Images/image6.jpg'
+    },
+    {
+      songName: 'Apna Bana Le',
+      songArtist: 'Arijit Singh',
+      songSrc: './Assets/songs/Apna Bana Le - Full Audio _ Bhediya _ Varun Dhawan, Kriti Sanon_ Sachin-Jigar,Arijit Singh,Amitabh B.webm',
+      songAvatar: './Assets/Images/image7.jpg'
+    }
+  ]
+
+  const handleNextSong = ()=>{
+    if (musicIndex >= musicAPI.length - 1) {
+      let setNumber = 0;
+      setMusicIndex(setNumber);
+      updateCurrentMusicDetails(setNumber);
+    }else{
+      let setNumber = musicIndex + 1;
+      setMusicIndex(setNumber)
+      updateCurrentMusicDetails(setNumber);
+    }
+  }
+
+  const handlePrevSong = ()=>{
+    if (musicIndex === 0) {
+      let setNumber = musicAPI.length - 1;
+      setMusicIndex(setNumber);
+      updateCurrentMusicDetails(setNumber);
+    }else{
+      let setNumber = musicIndex - 1;
+      setMusicIndex(setNumber)
+      updateCurrentMusicDetails(setNumber);
+    }
+  }
+
+  const updateCurrentMusicDetails = (number)=>{
+    let musicObject = musicAPI[number];
+    currentAudio.current.src = musicObject.songSrc;
+    currentAudio.current.play();
+    setCurrentMusicDetails({
+      songName: musicObject.songName,
+      songArtist: musicObject.songArtist,
+      songSrc: musicObject.songSrc,
+      songAvatar: musicObject.songAvatar
+    })
+    setIsAudioPlaying(true);
+  }
+
+
 }
 
 export default App;
